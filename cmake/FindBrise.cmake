379ad226ed31970b66e651eda9e884724ddb455e
Weng Xuetian <wengxt@gmail.com>
2012-07-24 07:53:20 -0400

    update FindBrise for better compatible

--- cmake/FindBrise.cmake
+++ cmake/FindBrise.cmake
@@ -4,13 +4,20 @@
 # BRISE_FOUND - System has brise package
 # BRISE_DIR - Brise absolute path
 
-if(IS_DIRECTORY /usr/share/brise)
-	set(BRISE_DIR /usr/share/brise)
-elseif(IS_DIRECTORY /usr/share/rime/brise)
-	set(BRISE_DIR /usr/share/rime/brise)
-else()
-	set(BRISE_FOUND FALSE)
-endif()
+set(BRISE_FIND_DIR "${PROJECT_SOURCE_DIR}/../brise"
+                   "${CMAKE_INSTALL_PREFIX}/share/brise"
+                   "${CMAKE_INSTALL_PREFIX}/share/rime/brise"
+                   "/usr/share/brise"
+                   "/usr/share/rime/brise")
+
+set(BRISE_FOUND FALSE)
+
+foreach(_BRISE_DIR ${BRISE_FIND_DIR})
+    if (IS_DIRECTORY ${_BRISE_DIR})
+        set(BRISE_FOUND True)
+        set(BRISE_DIR ${_BRISE_DIR})
+    endif (IS_DIRECTORY ${_BRISE_DIR})
+endforeach(_BRISE_DIR)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Brise DEFAULT_MSG
